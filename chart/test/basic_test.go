77bd72fe48dbf3cff75950590df4184a1d792d98
Moshe Immerman <moshe@flanksource.com>
2025-12-15 12:57:06 +0400

    chore: chart e2e test

---
+++ chart/test/basic_test.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/flanksource/clicky"
+	"github.com/flanksource/commons-test/helm"
+	"github.com/flanksource/commons/http"
+	"github.com/google/uuid"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Mission Control", ginkgo.Ordered, func() {
+	var mcStopChan, configDBStopChan chan struct{}
+
+	BeforeAll(func() {
+		By("Installing Mission Control")
+
+		Expect(helm.NewHelmChart(ctx, "../").
+			Release("mission-control").Namespace("mission-control").
+			WaitFor(time.Minute * 5).
+			Values(map[string]any{
+				"global": map[string]any{
+					"ui": map[string]any{
+						"host": "mission-control.cluster.local",
+					},
+				},
+				"authProvider": "basic",
+				"htpasswd": map[string]any{
+					"create": true,
+				},
+				"kratos": map[string]any{
+					"enabled": false,
+				},
+				"ingress": map[string]any{
+					"enabled": false,
+				},
+				"config-db": map[string]any{
+					"logLevel": "-vvv",
+				},
+				"logLevel": "-vvv",
+			}).
+			InstallOrUpgrade()).NotTo(HaveOccurred())
+
+		adminPasswordSecret, err := k8s.CoreV1().Secrets(namespace).Get(context.TODO(), "mission-control-admin-password", v1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred(), "Failed to get Mission Control admin password secret")
+		adminPassword := string(adminPasswordSecret.Data["password"])
+		Expect(adminPassword).NotTo(BeEmpty(), "Mission Control admin password should not be empty")
+		logger.Infof(clicky.MustFormat(adminPassword))
+
+		// Port forward to mission-control pod
+		var mcLocalPort int
+		mcLocalPort, mcStopChan, err = portForwardPod(ctx, namespace, "app.kubernetes.io/name=mission-control", 8080)
+		Expect(err).NotTo(HaveOccurred(), "Failed to port forward to Mission Control pod")
+
+		// Port forward to config-db pod
+		var configDBLocalPort int
+		configDBLocalPort, configDBStopChan, err = portForwardPod(ctx, namespace, "app.kubernetes.io/name=config-db", 8080)
+		Expect(err).NotTo(HaveOccurred(), "Failed to port forward to Config DB pod")
+
+		// Initialize Mission Control client using port-forwarded address
+		mcInstance = &MissionControl{
+			Client:   k8s,
+			HTTP:     http.NewClient().BaseURL(fmt.Sprintf("http://localhost:%d", mcLocalPort)).Auth("admin@local", adminPassword),
+			Username: "admin@local",
+			Password: adminPassword,
+			ConfigDB: http.NewClient().BaseURL(fmt.Sprintf("http://localhost:%d", configDBLocalPort)).Auth("admin@local", adminPassword),
+		}
+
+		mcInstanceWithoutAuth = &MissionControl{
+			Client:   k8s,
+			HTTP:     http.NewClient().BaseURL(fmt.Sprintf("http://localhost:%d", mcLocalPort)),
+			ConfigDB: http.NewClient().BaseURL(fmt.Sprintf("http://localhost:%d", configDBLocalPort)).Auth("admin@local", adminPassword),
+		}
+
+	})
+
+	AfterAll(func() {
+		if mcStopChan != nil {
+			close(mcStopChan)
+		}
+		if configDBStopChan != nil {
+			close(configDBStopChan)
+		}
+	})
+
+	It("Should be healthy", func() {
+		healthy, err := mcInstance.IsHealthy()
+		Expect(err).NotTo(HaveOccurred(), "Unable to query health endpoint")
+		Expect(healthy).To(BeTrue())
+
+		healthy, err = mcInstanceWithoutAuth.IsHealthy()
+		Expect(err).NotTo(HaveOccurred(), "Unable to query health endpoint")
+		Expect(healthy).To(BeTrue())
+	})
+
+	It("Should run WhoAmI", func() {
+		whoami, ok, err := mcInstance.WhoAmI()
+		Expect(err).NotTo(HaveOccurred(), "Unable to query whoami endpoint")
+		Expect(ok).To(BeTrue())
+		Expect(whoami["message"]).To(Equal("success"))
+
+		whoami, ok, err = mcInstanceWithoutAuth.WhoAmI()
+		Expect(err).NotTo(HaveOccurred(), "Unable to query whoami endpoint")
+		Expect(ok).To(BeFalse())
+		Expect(whoami["message"]).To(Equal("Unauthorized"))
+	})
+
+	It("Should run system scraper", func() {
+		scraper := mcInstance.GetScraper(uuid.Nil.String())
+		sr, err := scraper.Run()
+		Expect(err).NotTo(HaveOccurred())
+		logger.Infof("Scrape Result:\n%s", clicky.MustFormat(sr))
+
+		scraper = mcInstanceWithoutAuth.GetScraper(uuid.Nil.String())
+		sr, err = scraper.Run()
+		Expect(err).NotTo(HaveOccurred(), "basic auth")
+	})
+
+	// System scraper runs and should populate job histories/local agent
+	It("Should search catalog", func() {
+		results, err := mcInstance.SearchCatalog("type=*")
+		logger.Infof("%s", clicky.MustFormat(results))
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(results)).To(BeNumerically(">", 1))
+	})
+})
---
+++ chart/test/mission-control.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/flanksource/commons-db/kubernetes"
+	"github.com/flanksource/commons/http"
+)
+
+type MissionControl struct {
+	HTTP      *http.Client
+	ConfigDB  *http.Client
+	URL       string
+	Username  string
+	Password  string
+	Namespace string
+	*kubernetes.Client
+	DB *sql.DB
+}
+
+func (mc *MissionControl) POST(path string, body interface{}) (*http.Response, error) {
+	return mc.HTTP.R(context.TODO()).Post(path, body)
+}
+
+type Scraper struct {
+	mc   *MissionControl
+	Name string
+	Id   string
+}
+
+type ScrapeResults struct {
+	Response *http.Response
+	Error    error
+}
+
+func (mc *MissionControl) GetScraper(id string) *Scraper {
+	return &Scraper{
+		mc:   mc,
+		Id:   id,
+		Name: "",
+	}
+}
+
+type ScrapeResult struct {
+	Errors  []string       `json:"errors"`
+	Summary map[string]any `json:"scrape_summary"`
+}
+
+func (s *Scraper) Run() (*ScrapeResult, error) {
+	r, err := s.mc.ConfigDB.R(context.Background()).Post("/run/"+s.Id, map[string]string{"scraper": s.Name})
+	if err != nil {
+		return nil, err
+	}
+	result := &ScrapeResult{}
+	body, err := r.AsString()
+	if err != nil {
+		return nil, err
+	}
+	return result, json.Unmarshal([]byte(body), result)
+}
+
+type ResourceSelector struct {
+	ID            string            `json:"id,omitempty"`
+	Name          string            `json:"name,omitempty"`
+	Namespace     string            `json:"namespace,omitempty"`
+	Types         []string          `json:"types,omitempty"`
+	Statuses      []string          `json:"statuses,omitempty"`
+	Labels        map[string]string `json:"labels,omitempty"`
+	FieldSelector string            `json:"field_selector,omitempty"`
+	Search        string            `json:"search,omitempty"`
+}
+
+type SearchResourcesRequest struct {
+	Limit   int                `json:"limit,omitempty"`
+	Configs []ResourceSelector `json:"configs,omitempty"`
+}
+
+type SelectedResource struct {
+	ID         string            `json:"id,omitempty"`
+	Name       string            `json:"name,omitempty"`
+	Namespace  string            `json:"namespace,omitempty"`
+	Type       string            `json:"type,omitempty"`
+	Tags       map[string]string `json:"tags,omitempty"`
+	Config     string            `json:"config,omitempty"`
+	Labels     map[string]string `json:"labels,omitempty"`
+	Properties map[string]string `json:"properties,omitempty"`
+}
+
+type SearchResourcesResponse struct {
+	Configs []SelectedResource `json:"configs"`
+}
+
+func (mc *MissionControl) QueryCatalog(selector ResourceSelector) ([]SelectedResource, error) {
+	req := SearchResourcesRequest{
+		Configs: []ResourceSelector{selector},
+	}
+
+	r, err := mc.HTTP.R(context.TODO()).Post("/resources/search", req)
+	if err != nil {
+		return nil, err
+	}
+
+	if !r.IsOK() {
+		body, _ := r.AsString()
+		return nil, fmt.Errorf("query catalog failed: %s", body)
+	}
+
+	var response SearchResourcesResponse
+	if err := r.Into(&response); err != nil {
+		return nil, err
+	}
+
+	return response.Configs, nil
+}
+
+func (mc *MissionControl) SearchCatalog(search string) ([]SelectedResource, error) {
+	return mc.QueryCatalog(ResourceSelector{Search: search})
+}
+
+func (mc *MissionControl) IsHealthy() (bool, error) {
+	r, err := mc.HTTP.R(context.TODO()).Get("/health")
+	if err != nil {
+		return false, err
+	}
+
+	return r.IsOK(), nil
+}
+
+func (mc *MissionControl) WhoAmI() (map[string]any, bool, error) {
+	r, err := mc.HTTP.R(context.TODO()).Get("/auth/whoami")
+	if err != nil {
+		return nil, false, err
+	}
+
+	body, err := r.AsJSON()
+	return body, r.IsOK(), err
+}
