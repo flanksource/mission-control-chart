8a0f3308f574a98baf5747e9d27e9ae6b97fc1ec
Moshe Immerman <moshe@flanksource.com>
2025-12-15 12:57:06 +0400

    chore: chart e2e test

---
+++ chart/test/basic_test.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+
+	"github.com/flanksource/clicky"
+	"github.com/flanksource/commons-test/helm"
+	"github.com/flanksource/commons/http"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Mission Control", func() {
+
+	It("Basic Auth", func() {
+
+		By("Installing Mission Control")
+		mcChart = helm.NewHelmChart(ctx, "..")
+		// mcChart = helm.NewHelmChart(ctx, "flanksource/mission-control")
+		Expect(mcChart.
+			Release("mission-control").Namespace("mission-control").
+			Values(map[string]interface{}{
+				"global": map[string]interface{}{
+					"ui": map[string]interface{}{
+						"host": "mission-control.cluster.local",
+					},
+				},
+				"authProvider": "basic",
+
+				"htpasswd": map[string]interface{}{
+					"create": true,
+				},
+				"ingress": map[string]interface{}{
+					"enabled": true,
+					"annotations": map[string]interface{}{
+						"cert-manager.io/cluster-issuer": "self-signed",
+					},
+				},
+				"config-db": map[string]interface{}{
+					"logLevel": "-vvv",
+				},
+				"logLevel": "-vvv",
+			}).
+			InstallOrUpgrade()).NotTo(HaveOccurred())
+
+		adminPasswordSecret, err := k8s.CoreV1().Secrets(namespace).Get(context.TODO(), "mission-control-admin-password", v1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred(), "Failed to get Mission Control admin password secret")
+		adminPassword := string(adminPasswordSecret.Data["password"])
+		Expect(adminPassword).NotTo(BeEmpty(), "Mission Control admin password should not be empty")
+		logger.Infof(clicky.MustFormat(adminPassword))
+
+		podIP, err := k8s.GetPodIP(ctx, namespace, "app.kubernetes.io/name=mission-control")
+		Expect(err).NotTo(HaveOccurred(), "Failed to get Mission Control pod IP")
+
+		// Initialize Mission Control client, get credentianls and serviceIP from deployed chart.
+		mcInstance = &MissionControl{
+			Client:   k8s,
+			HTTP:     http.NewClient().BaseURL("http://"+podIP+":8080").Auth("admin@local", adminPassword),
+			Username: "admin@local",
+			Password: adminPassword,
+		}
+		podIP, err = k8s.GetPodIP(ctx, namespace, "app.kubernetes.io/name=config-db")
+		Expect(err).NotTo(HaveOccurred(), "Failed to get Config DB pod IPs")
+		mcInstance.ConfigDB = http.NewClient().BaseURL("http://"+podIP+":8080").Auth("admin@local", adminPassword)
+	})
+
+	It("SearchCatalog", func() {
+		results, err := mcInstance.SearchCatalog("type=*")
+		logger.Infof("%=v", results)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(len(results)).To(BeNumerically(">", 1))
+	})
+
+})
---
+++ chart/test/suite_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	flanksourceCtx "github.com/flanksource/commons-db/context"
+	"github.com/flanksource/commons-db/kubernetes"
+	"github.com/flanksource/commons-test/helm"
+	commonsLogger "github.com/flanksource/commons/logger"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	kubeconfig  string
+	namespace   string
+	chartPath   string
+	releaseName string
+	ctx         flanksourceCtx.Context
+)
+
+var logger commonsLogger.Logger
+var k8s *kubernetes.Client
+
+func findParentDir(dir string) string {
+	currentDir, _ := os.Getwd()
+
+	for {
+
+		if _, ok := os.Stat(filepath.Join(currentDir, dir)); ok == nil {
+			return filepath.Join(currentDir, dir)
+		}
+		if _, ok := os.Stat(filepath.Join(currentDir, ".git")); ok == nil {
+			// Reached the git root, stop searching
+			return currentDir
+		}
+		currentDir = filepath.Dir(currentDir)
+	}
+}
+
+func TestHelm(t *testing.T) {
+	logger = commonsLogger.NewWithWriter(GinkgoWriter)
+	commonsLogger.Use(GinkgoWriter)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Mission Control Helm Chart Suite")
+}
+
+var mcInstance *MissionControl
+var mcChart *helm.HelmChart
+var _ = BeforeSuite(func() {
+
+	// Get environment variables or use defaults
+	kubeconfig = os.Getenv("KUBECONFIG")
+	if kubeconfig == "" {
+		home := os.Getenv("HOME")
+		kubeconfig = filepath.Join(home, ".kube", "config")
+	}
+
+	namespace = os.Getenv("TEST_NAMESPACE")
+	if namespace == "" {
+		namespace = "mission-control"
+	}
+
+	chartPath = findParentDir("chart")
+
+	releaseName = "mission-control"
+
+	logger.Infof("KUBECONFIG=%s ns=%s, chart=%s", kubeconfig, namespace, chartPath)
+
+	if stat, err := os.Stat(kubeconfig); err != nil || stat.IsDir() {
+		path, _ := filepath.Abs(kubeconfig)
+		Skip(fmt.Sprintf("KUBECONFIG %s is not valid, skipping helm tests", path))
+	}
+
+	ctx = flanksourceCtx.New().
+		WithNamespace("mission-control")
+
+	var err error
+	k8s, err = ctx.LocalKubernetes(kubeconfig)
+	Expect(err).NotTo(HaveOccurred())
+
+})
